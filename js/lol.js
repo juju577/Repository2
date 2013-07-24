13314a2d8571cb36af1bab29c81dca5ef6f8e57d
juju577 <Misha172XDD@mail.ru>
2013-07-25 00:00:34 +0300

    modified:   js/lol.js

--- js/lol.js
+++ js/lol.js
@@ -1,11 +1,4 @@
 $(document).ready(function(){
- $(function() {
-    $( "#lol" ).accordion({
-      heightStyle: "content",
-      collapsible: true
-    });
-
-});
         $(function() {
             var toc = $("#toc").tocify({ selectors: "h2, h3, h4", extendPage:false}).data("toc-tocify");
             $(".optionName").popover({ trigger: "hover" });
