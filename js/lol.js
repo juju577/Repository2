ec4ad014910f27a4bcfb3c60967072c12c70b3c3
juju577 <Misha172XDD@mail.ru>
2013-07-24 20:30:44 +0300

    modified:   css/lol2.css 	modified:   index.html 	deleted:    js/jquery.tocify.js 	modified:   js/lol.js

--- js/lol.js
+++ js/lol.js
@@ -7,10 +7,20 @@ $(document).ready(function(){
 
 });
         $(function() {
-
             var toc = $("#toc").tocify({ selectors: "h2, h3, h4", extendPage:false}).data("toc-tocify");
-            prettyPrint();
             $(".optionName").popover({ trigger: "hover" });
 
         });
-	});
+        $("#button").click(function(){
+         $('#toc').each(function () {
+    var mylist = $(this);
+    var listitems = mylist.children('li').get();
+    listitems.sort(function(a, b) {
+    var compA = $(a).text().toUpperCase();
+    var compB = $(b).text().toUpperCase();
+    return (compA < compB) ? -1 : (compA > compB) ? 1 : 0;
+});
+$.each(listitems, function(idx, itm) { mylist.append(itm); });
+});
+});
+});
